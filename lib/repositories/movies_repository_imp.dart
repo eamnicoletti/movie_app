a83ab2a81e85b50979396b7f422726641cf83cbf
Emílio Nicoletti <eamnicoletti@gmail.com>
2022-12-22 21:46:59 -0300

    create repository implementation to getMovies method

---
+++ lib/repositories/movies_repository_imp.dart
@@ -0,0 +1,17 @@
+import 'package:movie_app/models/movies_model.dart';
+import 'package:movie_app/repositories/movies_repository.dart';
+import 'package:movie_app/utils/apis.utils.dart';
+
+import '../service/dio_service.dart';
+
+class MoviesRepositoryImp implements MoviesRepository {
+  final DioService _dioService;
+
+  MoviesRepositoryImp(this._dioService);
+
+  @override
+  Future<Movies> getMovies() async {
+    var result = await _dioService.getDio().get(API.REQUEST_MOVIE_LIST);
+    return Movies.fromJson(result.data);
+  }
+}
