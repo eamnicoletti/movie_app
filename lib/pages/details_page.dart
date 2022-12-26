73032cf0e26d539d9147db79f0b4004e4f87eb1c
Emílio Nicoletti <eamnicoletti@gmail.com>
2022-12-26 18:16:36 -0300

    create details page to show details movie

---
+++ lib/pages/details_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/models/movies_model.dart';
+
+import '../utils/apis.utils.dart';
+
+class DetailsPage extends StatelessWidget {
+  final Movie movie;
+  const DetailsPage({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.title!),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .55,
+                width: MediaQuery.of(context).size.width,
+                child: Hero(
+                  tag: movie.id!,
+                  child: Image.network(
+                    API.REQUEST_IMG(movie.posterPath!),
+                    loadingBuilder: (_, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return const Center(
+                          child: CircularProgressIndicator.adaptive());
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                movie.overview!,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: Icon(Icons.title),
+                  ),
+                  Text(
+                    movie.originalTitle!,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: Icon(Icons.new_releases),
+                  ),
+                  Text(movie.releaseDate!),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/custom_list_card_widget.dart
+++ lib/widgets/custom_list_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/models/movies_model.dart';
+import 'package:movie_app/pages/details_page.dart';
 
 import '../utils/apis.utils.dart';
 
@@ -9,50 +10,64 @@ class CustomListCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.black54,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(15),
-              bottomLeft: Radius.circular(15),
-            ),
-            child: Image.network(
-              API.REQUEST_IMG(movie.posterPath!),
-              loadingBuilder: (_, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return const Center(
-                    child: CircularProgressIndicator.adaptive());
-              },
-            ),
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (_) => DetailsPage(movie: movie),
+                  fullscreenDialog: true));
+        },
+        child: Container(
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.circular(15),
           ),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movie.title!,
-                  style: Theme.of(context).textTheme.headline6,
-                  softWrap: true,
-                  overflow: TextOverflow.visible,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  bottomLeft: Radius.circular(15),
                 ),
-                const Spacer(),
-                Text('Popularidade: ${movie.popularity}'),
-                const SizedBox(
-                  height: 10,
+                child: Hero(
+                  tag: movie.id!,
+                  child: Image.network(
+                    API.REQUEST_IMG(movie.posterPath!),
+                    loadingBuilder: (_, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return const Center(
+                          child: CircularProgressIndicator.adaptive());
+                    },
+                  ),
                 ),
-                Text('Votos: ${movie.voteAverage}'),
-              ],
-            ),
-          ))
-        ],
+              ),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title!,
+                      style: Theme.of(context).textTheme.headline6,
+                      softWrap: true,
+                      overflow: TextOverflow.visible,
+                    ),
+                    const Spacer(),
+                    Text('Popularidade: ${movie.popularity}'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text('Votos: ${movie.voteAverage}'),
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
